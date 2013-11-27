48d9651926913330598755e568d6b003a37b7e6a
wade12 <wadetaylor@engineer.com>
2013-11-27 13:18:57 +0000

    javascript interface: create a class with method and mark it as js interface method, then bind class containing method to the webview, then the js can access those methods through a js object.

--- WebViewTemplate/src/com/wade12/webviewtemplate/MainActivity.java
+++ WebViewTemplate/src/com/wade12/webviewtemplate/MainActivity.java
@@ -8,7 +8,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.view.Menu;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -26,6 +28,10 @@ public class MainActivity extends Activity {
 		setContentView(webView);
 		context = this;
 		// webView.setWebViewClient(client);
+		WebSettings webSettings = webView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		webView.addJavascriptInterface(new WebViewInterface(), "MainActivityInterface");
+		// webView is able to communicate with Activity through JavaScript.
 		
 		InputStream inputStream;
 		try {
@@ -77,4 +83,16 @@ public class MainActivity extends Activity {
 	*/
 	
 
+	// JavaScript enabling works as thus:
+	// create a class with methods and mark them as javascript interface methods
+	// then bind class containing methods to the webview
+	// and then the js can access those methods through a js object
+	public class WebViewInterface {
+		
+		@JavascriptInterface
+		public void dislayToast() {
+			Toast.makeText(context, "JavaScript WebInterface called.", Toast.LENGTH_LONG).show();
+		} // end method dislayToast
+	} // end Class webViewInterface
+	
 } // end Class MainActivity
